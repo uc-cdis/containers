e43b8479e8b6526320def6e627094ce0f0132708
George Thomas <george42@uchicago.edu>
2023-07-10 10:18:27 -0700

    feat(stata-license): remove copy_license and setup scripts from user-license

--- jupyter-pystata/dockerstart.sh
+++ jupyter-pystata/dockerstart.sh
@@ -8,12 +8,6 @@ else
   start-notebook.sh --NotebookApp.nbserver_extensions="{'custom_api.ready_handler':True}" $NOTEBOOK_ARGS $@ &
   JUPYTER_PID=$!
   sleep 10
-  # python3 /tmp/setup_notebooks.py
 
-  # echo "Deleting local copy of Stata license..."
-  # rm /usr/local/stata17/stata.lic
-
-  # echo "Signaling that Jupyter is ready to serve..."
-  # touch /tmp/ready
   wait $JUPYTER_PID
 fi
