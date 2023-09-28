c3f3bbac72c7f8513c0f84a13f618deca8eda7fc
Sai Shanmukha <nss10@outlook.com>
2023-09-28 18:16:37 -0500

    Make changes based on review comments

--- azlinux-jupyter-nextflow/resources/custom.js
+++
@@ -1,4 +0,0 @@
-// do not open notebooks in a new tab: https://github.com/jupyter/notebook/issues/4115
-define(['base/js/namespace'], function(Jupyter){
-    Jupyter._target = '_self';
-})
--- azlinux-jupyter-nextflow/resources/jupyter_notebook_config.py
+++
@@ -1,8 +0,0 @@
-import os
-
-c.NotebookApp.tornado_settings = {
-    "headers": {
-        "Content-Security-Policy": "frame-ancestors self %s"
-        % os.getenv("FRAME_ANCESTORS", "")
-    }
-}
