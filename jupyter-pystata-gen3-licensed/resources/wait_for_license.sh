63956d2eaaecddbfb445460ea8ab8dcdf49a7882
George Thomas <george42@uchicago.edu>
2023-12-20 10:42:46 -0800

    (HP-572) add conditional for license exists

--- jupyter-pystata-gen3-licensed/resources/wait_for_license.sh
+++ jupyter-pystata-gen3-licensed/resources/wait_for_license.sh
@@ -1,14 +1,15 @@
 # Signal to the distributor cron job that we want a license
 touch /tmp/waiting_for_license.flag
 
-# check contents of pod
-echo "Contents of data"
-ls -l /data
-echo "Contents of stata17"
-ls -l /usr/local/stata17
+if [ -f /data/stata.lic ]; then
+  mv /data/stata.lic /usr/local/stata17/stata.lic
+else
+  echo "Error: Missing license file /data/stata.lic"
+  # rm /tmp/waiting_for_license.flag
+  # exit 0
+fi
 
-cp /data/stata.lic /usr/local/stata17/stata.lic
-
+# TODO: remove this after the distribute-licenses job is disabled
 while [ ! -f /usr/local/stata17/stata.lic ]; do sleep 1; echo "Waiting for license."; done
 
 echo "Received a license. Starting jupyter."
