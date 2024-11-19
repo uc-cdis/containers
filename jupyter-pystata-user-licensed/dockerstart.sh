5187569618ac301d9392a818def91e47b91fad39
Mingfei Shao <mshao1@uchicago.edu>
2024-11-19 09:58:24 -0500

    update user licensed image build

--- jupyter-pystata-user-licensed/dockerstart.sh
+++ jupyter-pystata-user-licensed/dockerstart.sh
@@ -5,7 +5,7 @@ if [[ -z "${STATA_LICENSE}" ]]; then
 else
   echo $STATA_LICENSE > /usr/local/stata17/stata.lic
   unset STATA_LICENSE
-  start-notebook.sh --NotebookApp.nbserver_extensions="{'custom_api.ready_handler':True}" $NOTEBOOK_ARGS $@ &
+  start-notebook.sh --JupyterNotebookApp.nbserver_extensions="{'custom_api.ready_handler':True}" $NOTEBOOK_ARGS $@ &
   JUPYTER_PID=$!
   sleep 10
 
