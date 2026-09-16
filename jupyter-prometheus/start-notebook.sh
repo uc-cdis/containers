b6e740f75ac400ee3828d41feb0f5381e49ea6e5
Andrew Prokhorenkov <aprokh@uchicago.edu>
2026-09-16 12:54:55 -0500

    updates

--- jupyter-prometheus/start-notebook.sh
+++ jupyter-prometheus/start-notebook.sh
@@ -3,6 +3,5 @@ set -e  # Exit instantly if any single setup command fails
 
 /home/jovyan/.local/bin/jupyter lab \
     --KernelSpecManager.ensure_native_kernel=False \
-    --ServerApp.quit_button=False \
     --IdentityProvider.token="" \
     "$@"
